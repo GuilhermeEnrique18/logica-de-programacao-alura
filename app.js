f00cc8100ef09cc5074596e2526cdb67d266b7f7
GuilhermeDev <enriqueguilherme1209@gmail.com>
2025-04-03 14:53:09 -0300

    adicao comando break e correcao do portugues no fim do codigo

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 4;
-let chute = prompt('Escolha 1 número entre 1 e 10');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute = null;
+let tentativas = 1;
 
-if (chute  == numeroSecreto) {
-    alert(`acerto miseravi o numero foi: ${numeroSecreto}`);
-} else{
-    alert(`erro burro, era: ${numeroSecreto}`)
+// enquanto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas  = tentativas + 1;
+        tentativas++;
+
+    }
+}
+
+if (tentativas > 1){
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    }else{
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
 }
